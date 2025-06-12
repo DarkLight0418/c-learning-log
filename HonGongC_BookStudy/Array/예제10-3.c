bdd57deada6dec2ab481a07f4d66a271458dba38
DarkLightBegin23 <samseong0418@naver.com>
2025-06-12 13:31:23 +0900

    20250612 not easy...

--- HonGongC_BookStudy/Array/예제10-3.c
+++ HonGongC_BookStudy/Array/예제10-3.c
@@ -8,6 +8,7 @@ int main(void) {
     printf("배열의 값 : ");
     for (i = 0; i < 3; i++) {
         printf("%d ", *pa);  // pa가 가리키는 배열 요소 출력
+        scanf("%d", pa);  // pa가 가리키는 배열 요소에 입력, 간접 참조 연산 없음
         pa++;  // 다음 배열 요소를 가리키도록 pa 값 증가
     }
 
